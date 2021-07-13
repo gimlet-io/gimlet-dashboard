a7c9b9befe2d5fbf717074f9426f62fff330d93e
Laszlo Fogas <laszlo@laszlo.cloud>
2021-07-13 15:36:44 +0200

    Rendering services

--- web/src/components/services/services.jsx
+++ web/src/components/services/services.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import './services.css';
+import * as PropTypes from "prop-types";
 
 export default class Services extends Component {
   constructor(props) {
     super(props);
 
     // default state
+    let reduxState = this.props.store.getState();
     this.state = {
-      envs: {}
+      envs: reduxState.envs
     }
 
     // handling API and streaming state changes
@@ -30,7 +32,6 @@ export default class Services extends Component {
     return (
       <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
         {envs['staging'].stacks.map((service) => {
-          console.log(service);
           return (
             <li key={service.name} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
               <Service {...service}/>
@@ -39,9 +40,7 @@ export default class Services extends Component {
         })}
       </ul>
     );
-
   }
-
 }
 
 function Service(service) {
@@ -51,34 +50,42 @@ function Service(service) {
         <div className="flex items-center space-x-3">
           <h3 className="text-gray-900 text-sm font-medium truncate">{service.repo}</h3>
         </div>
-        {service.deployments.map((deployment) => (
-            <div>
-              <p className="text-xs">{deployment.namespace}/{deployment.name}
-                <span
-                  className="flex-shrink-0 inline-block px-2 py-0.5 mx-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                  {deployment.env}
-                </span>
-              </p>
-              <p className="mb-1">
-                <p className="break-words">{deployment.message}</p>
-                <p className="text-xs italic">
-                  <a
-                    href="https://github.com" target="_blank"
-                    rel="noopener noreferrer">
-                    {deployment.sha.slice(0, 6)}
-                  </a>
-                </p>
-              </p>
-              {deployment.pods.map((pod) => (
-                <Pod pod={pod}/>
-              ))}
-            </div>
-          )
-        )}
+        <Deployment deployment={service.deployment}/>
       </div>
     </div>
   )
 }
+
+function Deployment(props) {
+  const {deployment} = props;
+
+  return <div>
+    <p className="text-xs">{deployment.namespace}/{deployment.name}
+      <span
+        className="flex-shrink-0 inline-block px-2 py-0.5 mx-1 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+        {deployment.env}
+      </span>
+    </p>
+    <p className="mb-1">
+      <p className="break-words">{deployment.message}</p>
+      <p className="text-xs italic">
+        <a
+          href="https://github.com" target="_blank"
+          rel="noopener noreferrer">
+          {deployment.sha.slice(0, 6)}
+        </a>
+      </p>
+    </p>
+    {deployment.pods.map((pod) => (
+      <Pod pod={pod}/>
+    ))
+    }
+  </div>;
+}
+
+Deployment.propTypes = {
+  deployment: PropTypes.any,
+};
 
 function Pod(props) {
   const {pod} = props;
@@ -109,7 +116,7 @@ function Pod(props) {
   return (
     <span className="inline-block w-4 mr-1 mt-2">
       <svg viewBox="0 0 1 1"
-           className={`fill-current ${color}, ${pulsar}`}>
+           className={`fill-current ${color} ${pulsar}`}>
         <g>
           <title>{pod.name} - {pod.status}</title>
           <rect width="1" height="1"/>
