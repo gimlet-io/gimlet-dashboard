46de319df05bca1489717cf73983602532609764
Laszlo Fogas <laszlo@laszlo.cloud>
2021-07-22 14:32:24 +0200

    Search widget

--- web/src/components/nav/nav.jsx
+++ web/src/components/nav/nav.jsx
@@ -91,6 +91,24 @@ export default class Nav extends Component {
                     }
                   </div>
                 </div>
+                <div className="flex-1 flex items-center justify-center px-2 sm:ml-6 sm:justify-end">
+                  <div className="max-w-lg w-full lg:max-w-xs">
+                    <label htmlFor="search" className="sr-only">Search</label>
+                    <div className="relative">
+                      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                        <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                             fill="currentColor" aria-hidden="true">
+                          <path fill-rule="evenodd"
+                                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                                clip-rule="evenodd"/>
+                        </svg>
+                      </div>
+                      <input id="search" name="search"
+                             className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                             placeholder="Search" type="search"/>
+                    </div>
+                  </div>
+                </div>
                 <div className="hidden sm:ml-6 sm:flex sm:items-center">
                   {/*<button*/}
                   {/*  className="bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">*/}
