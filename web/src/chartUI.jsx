86aa68bf384b689566626fcc7e042b0f12814437
Szabó Gábor <gabor@gimlet.io>
2022-01-27 10:19:22 +0100

    Add null check for chartSchema and chartUISchema

--- web/src/chartUI.jsx
+++ web/src/chartUI.jsx
@@ -73,6 +73,14 @@ class ChartUI extends Component {
     const hasChange = nonDefaultValuesString !== '{ }' &&
       nonDefaultValuesString !== JSON.stringify(this.state.defaultState);
 
+    if (!this.state.chartSchema) {
+      return null;
+    }
+
+    if (!this.state.chartUISchema) {
+      return null;
+    }
+
     return (
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold leading-tight text-gray-900">{repoName}/envs/{env}
