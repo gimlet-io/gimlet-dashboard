8bdec3b98ed842929e08df5207d8a037ae2c96a6
Ádám <sisakadam@gmail.com>
2022-02-15 11:59:13 +0100

    Modified save and delete button

--- web/src/views/environments/EnvironmentCard.jsx
+++ web/src/views/environments/EnvironmentCard.jsx
@@ -1,17 +1,26 @@
-const EnvironmentCard = ({ singleEnv, isOnline }) => {
+const EnvironmentCard = ({ singleEnv, isOnline, deleteEnv }) => {
     return (
         <div className='bg-white overflow-hidden shadow rounded-lg my-4 w-fullpx-4 py-5 sm:px-6 focus:outline-none'>
-            <div className='inline-flex'>
-                <h3 className="text-lg leading-6 font-medium text-gray-900 pr-1">
-                    {singleEnv.name}
-                </h3>
-                {isOnline ?
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
-                    </svg> :
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414" />
-                    </svg>}
+            <div className='flex justify-between'>
+                <div className="inline-flex">
+                    <h3 className="text-lg leading-6 font-medium text-gray-900 pr-1">
+                        {singleEnv.name}
+                    </h3>
+                    {isOnline ?
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
+                        </svg> :
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414" />
+                        </svg>}
+                </div>
+                {!isOnline &&
+                    <div className="cursor-pointer" onClick={() => deleteEnv()}>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                    </div>
+                }
             </div>
         </div>
     )
--- web/src/views/environments/environments.jsx
+++ web/src/views/environments/environments.jsx
@@ -23,8 +23,9 @@ class Environments extends Component {
 
     getEnvironmentCards() {
         return (
-            Object.keys(this.state.envsFromDB).map(env => (<EnvironmentCard singleEnv={this.state.envsFromDB[env]} 
-            isOnline={this.state.envsFromDB[env].name === "staging"}
+            Object.keys(this.state.envsFromDB).map(env => (<EnvironmentCard singleEnv={this.state.envsFromDB[env]}
+                isOnline={this.state.envsFromDB[env].name === "staging"}
+                deleteEnv={this.delete}
             />))
         )
     }
@@ -52,20 +53,19 @@ class Environments extends Component {
                 <header>
                     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                         <h1 className="text-3xl font-bold leading-tight text-gray-900">Environments</h1>
-                        <input
-                        onChange={e => this.setState({ input: e.target.value})}
-                        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="environment" type="text" placeholder=""/>
-                       {this.state.input !== '' && <button
-                        onClick={() => this.save()}
-                        className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Save button</button>}
-                         <button
-                        onClick={() => this.delete()}
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete button</button>
                     </div>
                 </header>
                 <main>
                     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         <div className="px-4 py-8 sm:px-0">
+                            <input
+                                onChange={e => this.setState({ input: e.target.value })}
+                                class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="environment" type="text" placeholder="" />
+                            <button
+                                onClick={() => this.save()}
+                                className={(this.state.input === '' ? 'cursor-not-allowed bg-gray-500 hover:bg-gray-700 ' : 'bg-green-500 hover:bg-green-700 ') + `text-white font-bold my-2 py-2 px-4 rounded`}>
+                                Save environment
+                            </button>
                             {this.getEnvironmentCards()}
                         </div>
                     </div>
