c02f1dad7ff784854b62ebe6b7fee857d1c5cda3
szabogabor91 <91szabogabor@gmail.com>
2022-02-17 15:20:40 +0100

    Change 'className' to 'class' in export + autoformat

--- web/src/components/deployStatus/deployStatus.jsx
+++ web/src/components/deployStatus/deployStatus.jsx
@@ -1,9 +1,9 @@
-import {Component, Fragment} from 'react'
-import {Transition} from '@headlessui/react'
-import {XIcon} from '@heroicons/react/solid'
-import {ACTION_TYPE_CLEAR_DEPLOY_STATUS} from "../../redux/redux";
+import { Component, Fragment } from 'react'
+import { Transition } from '@headlessui/react'
+import { XIcon } from '@heroicons/react/solid'
+import { ACTION_TYPE_CLEAR_DEPLOY_STATUS } from "../../redux/redux";
 
-export default className DeployStatus extends Component {
+export default class DeployStatus extends Component {
   constructor(props) {
     super(props);
 
@@ -18,13 +18,13 @@ export default className DeployStatus extends Component {
     this.props.store.subscribe(() => {
       let reduxState = this.props.store.getState();
 
-      this.setState({runningDeploys: reduxState.runningDeploys});
-      this.setState({settings: reduxState.settings});
+      this.setState({ runningDeploys: reduxState.runningDeploys });
+      this.setState({ settings: reduxState.settings });
     });
   }
 
   render() {
-    const {runningDeploys, settings} = this.state;
+    const { runningDeploys, settings } = this.state;
     const gitopsRepo = settings.gitopsRepo;
 
     if (runningDeploys.length === 0) {
@@ -35,7 +35,7 @@ export default className DeployStatus extends Component {
 
     let gitopsWidget = (
       <div className="mt-2">
-        <Loading/>
+        <Loading />
       </div>
     )
     let appliedWidget = null;
@@ -74,7 +74,7 @@ export default className DeployStatus extends Component {
         </div>
       )
       appliedWidget = (
-        <Loading/>
+        <Loading />
       )
     }
 
@@ -142,29 +142,29 @@ export default className DeployStatus extends Component {
                   <div className="flex">
                     <div className="w-0 flex-1 justify-between">
                       {!deploy.rollback &&
-                      <p className="text-yellow-100 font-semibold">
-                        Rolling out {deploy.app}
-                      </p>
+                        <p className="text-yellow-100 font-semibold">
+                          Rolling out {deploy.app}
+                        </p>
                       }
                       {deploy.rollback &&
-                      <p className="text-yellow-100 font-semibold">
-                        Rolling back {deploy.app}
-                      </p>
+                        <p className="text-yellow-100 font-semibold">
+                          Rolling back {deploy.app}
+                        </p>
                       }
                       <p className="pl-2  ">
                         🎯 {deploy.env}
                       </p>
                       {!deploy.rollback &&
-                      <p className="pl-2">
-                        <span>📎</span>
-                        <a
-                          href={`https://github.com/${deploy.repo}/commit/${deploy.sha}`}
-                          target="_blank" rel="noopener noreferrer"
-                          className='ml-1'
-                        >
-                          {deploy.sha.slice(0, 6)}
-                        </a>
-                      </p>
+                        <p className="pl-2">
+                          <span>📎</span>
+                          <a
+                            href={`https://github.com/${deploy.repo}/commit/${deploy.sha}`}
+                            target="_blank" rel="noopener noreferrer"
+                            className='ml-1'
+                          >
+                            {deploy.sha.slice(0, 6)}
+                          </a>
+                        </p>
                       }
                       {gitopsWidget}
                       <div className='pl-2 mt-4'>{appliedWidget}</div>
@@ -179,7 +179,7 @@ export default className DeployStatus extends Component {
                         }}
                       >
                         <span className="sr-only">Close</span>
-                        <XIcon className="h-5 w-5" aria-hidden="true"/>
+                        <XIcon className="h-5 w-5" aria-hidden="true" />
                       </button>
                     </div>
                   </div>
@@ -195,13 +195,13 @@ export default className DeployStatus extends Component {
 
 function
 
-Loading() {
+  Loading() {
   return (
     <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-         viewBox="0 0 24 24">
+      viewBox="0 0 24 24">
       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
       <path className="opacity-75" fill="currentColor"
-            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
     </svg>
   )
 }
