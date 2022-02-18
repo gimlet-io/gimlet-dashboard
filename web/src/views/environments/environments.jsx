b13ec34a96537cd440d81a5d04dc012047d9d34d
szabogabor91 <91szabogabor@gmail.com>
2022-02-18 10:12:20 +0100

    Add 'Create' environment' to card without toggle

--- web/src/views/environments/environments.jsx
+++ web/src/views/environments/environments.jsx
@@ -17,7 +17,6 @@ class Environments extends Component {
             hasRequestError: false,
             saveButtonTriggered: false,
             hasSameEnvNameError: false,
-            addEnvMenuIsOpen: false
         };
         this.props.store.subscribe(() => {
             let reduxState = this.props.store.getState();
@@ -123,27 +122,26 @@ class Environments extends Component {
                                 <EnvironmentsPopUpWindow
                                     hasRequestError={this.state.hasRequestError} />}
                             {this.getEnvironmentCards()}
-                            <h1 onClick={() =>
-                                this.setState({ addEnvMenuIsOpen: !this.state.addEnvMenuIsOpen })}
-                                className="inline-block cursor-pointer select-none">
-                                Add environment
-                            </h1>
-                            {this.state.addEnvMenuIsOpen &&
-                                <>
+                            <div className="bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200">
+                                <div className="px-4 py-5 sm:px-6">
+                                    <h3 className="text-lg leading-6 font-medium text-gray-900">Create environment</h3>
+                                </div>
+                                <div className="px-4 py-5 sm:px-6">
                                     <input
                                         onChange={e => this.setState({ input: e.target.value })}
-                                        className="shadow appearance-none border rounded w-full my-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="environment" type="text" value={this.state.input} placeholder="Please enter an environment name..." />
+                                        className="shadow appearance-none border rounded w-full my-4 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="environment" type="text" value={this.state.input} placeholder="Please enter an environment name" />
                                     <div className="p-0 flow-root">
                                         <span className="inline-flex rounded-md shadow-sm gap-x-3 float-right">
                                             <button
                                                 disabled={this.state.input === "" || this.state.saveButtonTriggered}
                                                 onClick={() => this.save()}
                                                 className={(this.state.input === "" || this.state.saveButtonTriggered ? 'bg-gray-600 cursor-not-allowed' : 'bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-indigo active:bg-green-700') + ` inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white transition ease-in-out duration-150`}>
-                                                Save
+                                                Create
                                             </button>
                                         </span>
                                     </div>
-                                </>}
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </main>
