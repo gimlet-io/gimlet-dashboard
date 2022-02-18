b5c7b6640b91a6241079487bf5fa1b0b47b25df6
szabogabor91 <91szabogabor@gmail.com>
2022-02-18 09:20:07 +0100

    Fix typo in saveButtonTriggered condition

--- web/src/views/environments/environments.jsx
+++ web/src/views/environments/environments.jsx
@@ -134,7 +134,7 @@ class Environments extends Component {
                                             <button
                                                 disabled={this.state.input === "" || this.state.saveButtonTriggered}
                                                 onClick={() => this.save()}
-                                                className={(this.state.input === "" || this.statesaveButtonTriggered ? 'bg-gray-600 cursor-not-allowed' : 'bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-indigo active:bg-green-700') + ` inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white transition ease-in-out duration-150`}>
+                                                className={(this.state.input === "" || this.state.saveButtonTriggered ? 'bg-gray-600 cursor-not-allowed' : 'bg-green-600 hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-indigo active:bg-green-700') + ` inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white transition ease-in-out duration-150`}>
                                                 Save
                                             </button>
                                         </span>
