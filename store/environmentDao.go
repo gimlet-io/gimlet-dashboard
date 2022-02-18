b97f191686cd182e29628a3b4357e0c0ef6c8d46
szabogabor91 <91szabogabor@gmail.com>
2022-02-18 13:19:30 +0100

    Delete redundant commented out function

--- store/environmentDao.go
+++ store/environmentDao.go
@@ -10,12 +10,6 @@ func (db *Store) CreateEnvironment(environment *model.Environment) error {
 	return meddler.Insert(db, "environments", environment)
 }
 
-// func (db *Store) GetEnvironment(name string) (*model.Environment, error) {
-// 	data := new(model.Environment)
-// 	err := meddler.QueryRow(db, data, "SELECT * FROM environments WHERE name = $1", name)
-// 	return data, err
-// }
-
 func (db *Store) GetAllEnvironment() ([]*model.Environment, error) {
 	stmt := sql.Stmt(db.driver, sql.SelectEnvironment)
 	data := []*model.Environment{}
