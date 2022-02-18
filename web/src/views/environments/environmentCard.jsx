de81a15efbc8636e85aab95002aae31cf8c7b8d2
Ádám <sisakadam@gmail.com>
2022-02-18 10:38:52 +0100

    Replace isOnline svg

--- web/src/views/environments/environmentCard.jsx
+++ web/src/views/environments/environmentCard.jsx
@@ -6,13 +6,14 @@ const EnvironmentCard = ({ isOnline, singleEnv, deleteEnv }) => {
                     <h3 className="text-lg leading-6 font-medium text-gray-900 pr-1">
                         {singleEnv.name}
                     </h3>
-                    {isOnline ?
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z" />
-                        </svg> :
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.364 5.636a9 9 0 010 12.728m0 0l-2.829-2.829m2.829 2.829L21 21M15.536 8.464a5 5 0 010 7.072m0 0l-2.829-2.829m-4.243 2.829a4.978 4.978 0 01-1.414-2.83m-1.414 5.658a9 9 0 01-2.167-9.238m7.824 2.167a1 1 0 111.414 1.414m-1.414-1.414L3 3m8.293 8.293l1.414 1.414" />
-                        </svg>}
+                    <span title={isOnline ? 'Connected' : 'Disconnected'}>
+                        <svg className={(isOnline ? 'text-teal-400' : 'text-red-400') + ' inline fill-current ml-1'} xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 20 20">
+                            <path
+                                d="M0 14v1.498c0 .277.225.502.502.502h.997A.502.502 0 0 0 2 15.498V14c0-.959.801-2.273 2-2.779V9.116C1.684 9.652 0 11.97 0 14zm12.065-9.299l-2.53 1.898c-.347.26-.769.401-1.203.401H6.005C5.45 7 5 7.45 5 8.005v3.991C5 12.55 5.45 13 6.005 13h2.327c.434 0 .856.141 1.203.401l2.531 1.898a3.502 3.502 0 0 0 2.102.701H16V4h-1.832c-.758 0-1.496.246-2.103.701zM17 6v2h3V6h-3zm0 8h3v-2h-3v2z"
+                            />
+                        </svg>
+                    </span>
+
                 </div>
                 {!isOnline &&
                     <div className="inline-flex">
